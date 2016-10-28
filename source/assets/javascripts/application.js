907123ebc0638f8a597f8457bef86cf8d0304f3c
Eric Gardner <gardner.ec@gmail.com>
2016-10-28 10:52:29 -0700

    Set up masonry layouts via Salvattore.js
    This adds a left-to-right masonry grid to the site using Salvattore.js.
    This offers one significant advantage over the CSS3 columns approach
    because content is ordered left-to-right horizonally instead of
    descending down each row; this is generally what people expect.
    
    In some cases (TF page for now) the other approach is still used (order
    doesn't matter here), but in both cases the grid spacing is the same.
    Salvattore needs negative margins in order to produce even columns, btw.

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,27 +1,31 @@
-//= require_tree .
+//= require smoothstate.min
+//= require jquery.superslides.min
+//= require video.min
+//= require ui
+
 
 $(document).ready(function() {
   menuSetup();
   slideSetup();
 
   // smoothState init
-  // $("#main").smoothState({
-    // onStart: {
-      // duration: 400,
-      // render: function($container) {
-        // $container.animate('fadeOut', { duration: 200 });
-      // }
-    // },
-    // onReady: {
-      // duration: 400,
-      // render: function($container, $newContent) {
-        // $container.html($newContent);
-        // $container.animate('fadeIn', { duration: 100 });
-      // }
-    // },
-    // onAfter: function($container, $newContent) {
-      // menuSetup();
-      // slideSetup();
-    // }
-  // });
+  $("#main").smoothState({
+    onStart: {
+      duration: 400,
+      render: function($container) {
+        $container.animate('fadeOut', { duration: 200 });
+      }
+    },
+    onReady: {
+      duration: 400,
+      render: function($container, $newContent) {
+        $container.html($newContent);
+        $container.animate('fadeIn', { duration: 100 });
+      }
+    },
+    onAfter: function($container, $newContent) {
+      menuSetup();
+      slideSetup();
+    }
+  });
 });
